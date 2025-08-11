ef7747d4afa7844cf90a9057c400d8e6c300f9a8
Babu <babu@gmail.com>
2025-08-11 11:28:38 +0530

    file-name-error-fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB= async()=>{
     try{
         mongoose.connection.on('connected',()=>{console.log("MongoDB Connected")})
-        //mongoose.connection.on('connecting',()=>{console.log("Connecting to data base now")})
+       
         await mongoose.connect(`${process.env.MONGODB_URL}/pingUp`)
     }
     catch(err){
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { use } from "react";
 
 const userSchema=new mongoose.Schema({
      _id:{type:String ,required:true},
@@ -10,9 +9,9 @@ const userSchema=new mongoose.Schema({
         profile_picture:{type:String ,default:''},
          cover_photo:{type:String ,default:''},
          location:{type:String,default:''},
-         followers:[{type:String,ref:'user'}],
-         following:[{type:String,ref:'user'}],
-         connections:[{type:String,ref:'user'}]
+         followers:[{type:String,ref:'User'}],
+         following:[{type:String,ref:'User'}],
+         connections:[{type:String,ref:'User'}]
 },{timestamps:true,minimize:false})
 const User=mongoose.model('User',userSchema);
 export default User
